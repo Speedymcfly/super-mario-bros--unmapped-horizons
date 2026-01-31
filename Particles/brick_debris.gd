22fa00e47ea1a2980b51b438c9cb473d3f02e009
Dia-lock64 <diamologh@gmail.com>
2026-01-30 18:57:01 -0600

    Update brick_debris.gd

--- Particles/brick_debris.gd
+++ Particles/brick_debris.gd
@@ -10,5 +10,7 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+	if not Globals.is_onscreen(global_position):
+		queue_free()
 
 	move_and_slide()
